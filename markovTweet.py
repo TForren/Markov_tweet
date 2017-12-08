25a3b6e5c2baef1fd69e24555092b3be95eb867b
Teague <tforren@udel.edu>
2017-12-08 15:04:33 -0500

    made it in python in 1 hour because python is superior

---
+++ markovTweet.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+import random
+import time
+filename = "frank10.txt"
+
+f = open(filename, 'r')
+
+markovMatrix = defaultdict(dict)
+
+starters = []
+enders = []
+
+for line in f:
+    splitLine = line.replace(",","").split()
+    i = 0
+    while i < len(splitLine) - 1:
+
+        #get 2 adjacent words
+        first = splitLine[i]
+        second = splitLine[i+1]
+        #check for edge words
+        if "." in first:
+            starters += [second]
+            enders += [first]
+            i += 1
+            continue
+        elif "." in second:
+            enders += [second]
+
+        #add to adjacent word matrix
+        if first in markovMatrix.keys():
+            if second in markovMatrix.get(first):
+                markovMatrix[first][second] += 1
+            else:
+                markovMatrix[first][second] = 1
+        else:
+            markovMatrix[first][second] = 1
+        i += 1
+
+tweet = [random.choice(starters)]
+for i in range(0,280):
+
+    if markovMatrix.get(tweet[i]) == None:
+        tweet += "."
+        tweet += [random.choice(starters)]
+        break
+    possible = markovMatrix.get(tweet[i]).keys()
+    nextWord = possible[random.randint(0,len(possible)-1)]
+    tweet += [nextWord]
+    if nextWord in enders:
+        break
+
+print " ".join(tweet)
